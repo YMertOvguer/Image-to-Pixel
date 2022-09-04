2d349b37dc2fbabc69cbea72d81b7bab2bb17a89
YMertOvguer <112813253+YMertOvguer@users.noreply.github.com>
2022-09-04 19:33:36 +0300

    Add files via upload

---
+++ ImgToPixel.py
@@ -0,0 +1,24 @@
+import cv2
+
+# Input image
+img = cv2.imread('messi.jpg')
+
+# Get input size
+height, width = img.shape[:2]
+
+# Desired "pixelated" size
+w, h = (64, 64)
+
+# Resize input to "pixelated" size
+temp = cv2.resize(img, (w, h), interpolation=cv2.INTER_LINEAR)
+
+# Initialize output image
+output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+
+cv2.imshow('Input', img)
+cv2.imshow('Output', output)
+
+cv2.imwrite("PixelLena.png", output)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
